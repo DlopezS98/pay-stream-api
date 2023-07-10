a08ea2428244cd5bda9b927f21679759b0e1a1e0
DlopezS98 <01dlopezs98@gmail.com>
2023-07-09 22:28:52 -0600

    inital setup for tsconfig and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   root: true,
   env: {
@@ -21,5 +22,19 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-useless-constructor': ['error'],
+    '@typescript-eslint/object-curly-spacing': ['error', 'always'],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/explicit-function-return-type': ['error'],
+    'prettier/prettier': ['error', { 'endOfLine': 'auto' }],
+    // 'sort-imports': ['error',
+    //   {
+    //     ignoreCase: false,
+    //     ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
+    //     ignoreMemberSort: false,
+    //     memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+    //     allowSeparatedGroups: true,
+    //   },
+    // ]
   },
 };
