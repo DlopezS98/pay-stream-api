754e2046c26b40f723d46e0e56fb0ff81388d7c7
DlopezS98 <01dlopezs98@gmail.com>
2023-07-09 22:34:05 -0600

    eslint plugin - sort imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+// Sort Imports: https://medium.com/weekly-webtips/how-to-sort-imports-like-a-pro-in-typescript-4ee8afd7258a
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -10,6 +12,8 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   root: true,
   env: {
@@ -27,14 +31,26 @@ module.exports = {
     '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
     '@typescript-eslint/explicit-function-return-type': ['error'],
     'prettier/prettier': ['error', { 'endOfLine': 'auto' }],
-    // 'sort-imports': ['error',
-    //   {
-    //     ignoreCase: false,
-    //     ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
-    //     ignoreMemberSort: false,
-    //     memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-    //     allowSeparatedGroups: true,
-    //   },
-    // ]
+    
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin', // Built-in imports (come from NodeJS native) go first
+          'external', // <- External imports
+          'internal', // <- Absolute imports
+          ['sibling', 'parent'], // <- Relative imports, the sibling and parent types they can be mingled together
+          'index', // <- index imports
+          'unknown', // <- unknown
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          /* sort in ascending order. Options: ["ignore", "asc", "desc"] */
+          order: 'asc',
+          /* ignore case. Options: [true, false] */
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
 };
